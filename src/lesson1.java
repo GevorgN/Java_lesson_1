787db483201045f4ea5ddc6df5b3f878464a4b71
Gevorg7 <64933888+Gevorg7@users.noreply.github.com>
2021-03-25 21:15:57 +0400

    First commit

---
+++ src/lesson1.java
@@ -0,0 +1,22 @@
+public class lesson1 {
+    public static void main(String[] args) {
+        byte a = 10;
+        System.out.println(a);
+        short b = 350;
+        System.out.println(b);
+        int c = 1400;
+        System.out.println(c);
+        long d = 15000000000l;
+        System.out.println(d);
+        double e = 156.756;
+        System.out.println(e);
+        float f = 17896.456f;
+        System.out.println(f);
+        boolean t = true;
+        System.out.println(t);
+        char ch = 'c';
+        System.out.println(ch);
+        String str = "Hello world!";
+        System.out.println(str);
+    }
+}
